ef7638b777d7fc850c797787bfffa15a0ef3a793
Juan P. Dantur <jpdantur@gmail.com>
2018-04-10 10:54:27 -0300

    Analytical Solution

---
+++ Part1.m
@@ -0,0 +1,11 @@
+%Solucion analitica
+dt = 0.001; %Probar distintos valores
+m = 70;
+k = 10000;
+gama = 100;
+tf=5;
+
+x = 0:dt:tf;
+y = exp(-(gama/(2*m))*x).*cos(sqrt(k/m - gama^2/(4*m^2))*x);
+plot(x,y)
+%%%
