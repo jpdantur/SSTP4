25360bd9eff8c2dd957798105de7bf5a6abdf04b
Juan P. Dantur <jpdantur@gmail.com>
2018-04-19 16:12:29 -0300

    Merge conflict

--- src/main/java/ar/edu/itba/ss/Part3.java
+++ src/main/java/ar/edu/itba/ss/Part3.java
@@ -2,6 +2,7 @@ package ar.edu.itba.ss;
 
 import ar.edu.itba.ss.utils.PlanetPositionReader;
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+import org.apache.commons.math3.util.FastMath;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,8 +18,8 @@ public class Part3 {
     }
 
     private Vector2D getShipLocation() {
-        //return planets.get(Planet.Earth).getPosition().;
-        return null;
+        double angle = Vector2D.angle(new Vector2D(1,0),planets.get(Planet.Earth).getPosition());
+        return planets.get(Planet.Earth).getPosition().add(1500,new Vector2D(FastMath.cos(angle),FastMath.sin(angle)));
     }
 
 
