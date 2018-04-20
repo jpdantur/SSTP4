ad4c380aaa2096ea41892fe77981948434111ef2
scamisay <scamisay@itba.edu.ar>
2018-04-20 12:33:54 -0300

    ejercicio 1: graficos del ecm

--- src/main/java/ar/edu/itba/ss/graphics/exercise1/ECMComparison.java
+++ src/main/java/ar/edu/itba/ss/graphics/exercise1/ECMComparison.java
@@ -9,15 +9,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class ECMComparison {
 
     public static void main(String[] args){
-        List<Double> dtList = Arrays.asList(0.01, 0.02, 0.25, 0.03, 0.5);
+        List<Double> dtList = Arrays.asList(0.001, 0.005, 0.01, 0.02, 0.025, 0.03, 0.05);
         List<Function<Double, Oscillation>> methods = Arrays.asList(LeapFrog::new, BeemanPC::new, GearPC::new);
 
-        List<List<Double>> ecms = methods.stream().map( m -> new ECM(dtList, m, 5).calculateECMs()).collect(toList());
+        String dtValues = dtList.stream().map(x -> x.toString()).collect(joining(", "));
+        List<List<Double>> ecms =
+                methods.stream().map(
+                        m -> new ECM(dtList, m, 5)
+                            .calculateECMs()
+                ).collect(toList());
+        List<String> values = ecms.stream().map(l -> l.stream()
+                .map( x -> x.toString())
+                .collect(joining(", "))
+        ).collect(toList());
         System.out.println(ecms);
     }
 
